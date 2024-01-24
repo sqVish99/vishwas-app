124dede615e23202ae28b9d65c136aee51b662e5
sqVish99 <vmharish@asu.edu>
2024-01-23 23:40:24 -0700

    adding hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Projects from './pages/Projects/Projects';
@@ -8,7 +8,7 @@ import Journey from './pages/Journey/Journey';
 import Footer from './components/Footer/Footer';
 
 const App = () => (
-  <Router basename='vishwas-app'>
+  <HashRouter basename='vishwas-app'>
     <Header />
     <Routes>
       <Route path="/home" element={<Home />} />
@@ -17,7 +17,7 @@ const App = () => (
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
     <Footer />
-  </Router>
+  </HashRouter>
 );
 
 export default App;
