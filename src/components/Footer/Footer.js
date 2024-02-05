eb10c1f0a5f0be4fb339ba4e3e86cd13b32f665f
sqVish99 <vmharish@asu.edu>
2024-02-04 19:36:36 -0700

    footer modification

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Footer.css';
 import email from '../../assets/images/email.svg';
 import linkedin from '../../assets/images/linkedin.svg';
+import github from '../../assets/images/github.svg';
 
 const Footer = () => {
     return (
@@ -9,6 +10,7 @@ const Footer = () => {
             <div className="footer-content">
                 <a href="mailto:vmharish@asu.edu"><img className="footer-logo" src={email} alt='email' /></a>
                 <a href="https://www.linkedin.com/in/vishwas-m-h/" target="_blank" rel="noopener noreferrer"><img src={linkedin} className="footer-logo" alt='linkedin' /></a>
+                <a className="github-icon" href="https://github.com/sqVish99" target="_blank" rel="noopener noreferrer"><img src={github} className="footer-logo" alt='github' /></a>
             </div>
         </div>
     );
