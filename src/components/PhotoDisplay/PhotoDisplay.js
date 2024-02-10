920ecc52f527ef2c2574fc1798fb7582c680abc0
sqVish99 <vmharish@asu.edu>
2024-02-10 00:02:35 -0700

    auto slideshow

--- src/components/PhotoDisplay/PhotoDisplay.js
+++ src/components/PhotoDisplay/PhotoDisplay.js
@@ -1,48 +1,55 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './PhotoDisplay.css';
 import Leftarrow from '../../assets/images/Leftarrow.js';
 import Rightarrow from '../../assets/images/Rightarrow.js';
 
 const PhotoDisplay = ({ images }) => {
-   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-   const [swipeHandled, setSwipeHandled] = useState(false);
-   const touchStartX = useRef(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [swipeHandled, setSwipeHandled] = useState(false);
+  const touchStartX = useRef(0);
 
-   const nextImage = () => {
+  useEffect(() => {
+    const timer = setInterval(() => {
       setCurrentImageIndex((currentImageIndex + 1) % images.length);
-   };
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [currentImageIndex, images]);
 
-   const prevImage = () => {
-      setCurrentImageIndex((currentImageIndex - 1 + images.length) % images.length);
-   };
+  const nextImage = () => {
+    setCurrentImageIndex((currentImageIndex + 1) % images.length);
+  };
 
-   const handleTouchStart = (e) => {
-      touchStartX.current = e.touches[0].clientX;
-    };
-    
-    const handleTouchMove = (e) => {
-      if (!swipeHandled) {
-        if (touchStartX.current - e.touches[0].clientX > 100) {
-          nextImage();
-          setSwipeHandled(true);
-        } else if (touchStartX.current - e.touches[0].clientX < -100) {
-          prevImage();
-          setSwipeHandled(true);
-        }
+  const prevImage = () => {
+    setCurrentImageIndex((currentImageIndex - 1 + images.length) % images.length);
+  };
+
+  const handleTouchStart = (e) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchMove = (e) => {
+    if (!swipeHandled) {
+      if (touchStartX.current - e.touches[0].clientX > 100) {
+        nextImage();
+        setSwipeHandled(true);
+      } else if (touchStartX.current - e.touches[0].clientX < -100) {
+        prevImage();
+        setSwipeHandled(true);
       }
-    };
-    
-    const handleTouchEnd = () => {
-      setSwipeHandled(false);
-    };
+    }
+  };
 
-   return (
-      <div className="photo-display" onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
-         <button className="nav-button prev-button" onClick={prevImage}><Leftarrow /></button>
-         <img className="displayed-image" src={images[currentImageIndex]} alt="" />
-         <button className="nav-button next-button" onClick={nextImage}><Rightarrow /></button>
-      </div>
-   );
+  const handleTouchEnd = () => {
+    setSwipeHandled(false);
+  };
+
+  return (
+    <div className="photo-display" onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
+      <button className="nav-button prev-button" onClick={prevImage}><Leftarrow /></button>
+      <img className="displayed-image" src={images[currentImageIndex]} alt="" />
+      <button className="nav-button next-button" onClick={nextImage}><Rightarrow /></button>
+    </div>
+  );
 };
 
 export default PhotoDisplay;
