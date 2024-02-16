d040fe9b676beae9b1792053252a302f8d6a0390
sqVish99 <vmharish@asu.edu>
2024-02-15 23:41:16 -0700

    resume update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,22 +6,23 @@ const Home = () => (
   <section id="Home">
     <div className="container">
       <h1>
-        Hi there!
+        Hi!
         {/* <div className="react-logo-container">
           <img className="react-logo" src={logo} alt="React Logo" />
         </div> */}
       </h1>
       <div className="about">
-        <p className="about-thanks">Thank you for visting my page!</p>
         <p className="about-text">
-          I'm Vishwas. I'm an ambitious Master's student majoring in Computer Science at Arizona State University,
-          on track to graduate in May 2025. Armed with a robust background in Computer Science and Engineering,
-          I actively contribute to software development, specializing in elevating application performance.
-          Beyond academia, I channel my passion into community service, guiding underprivileged children,
-          and spearheading impactful fundraising events. Proficient in a diverse array of cutting-edge programming languages
-          and technologies, I bring a dynamic skill set to the table.
-          Fluent in English, Hindi, and Kannada, I integrate technical expertise with strong communication and leadership skills.
-          Outside of tech, I enjoy going on long bicycle rides, hiking, photography and reading books.
+          Welcome to my portfolio! I'm a dedicated Computer Science Master's student at ASU, 
+          with a deep passion for crafting innovative software solutions. 
+          My journey has been a blend of academic excellence, practical project work, 
+          and a continuous quest for knowledge in AI, web development, and beyond. 
+          Through this website, I share my projects, insights, 
+          and experiences that have shaped my approach to technology and problem-solving. 
+          Whether you're a fellow tech enthusiast, a potential collaborator, or an employer looking for talent, 
+          I invite you to explore my work and discover the impact we can make together.
+          Aside from my professional life, I love cycling, hiking, and reading books.
+          I love giving back to the community and I have been a part of various volunteering activities.
         </p>
       </div>
     </div>
