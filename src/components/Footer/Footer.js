0ee27536b90fb8ec6a3d5e0e5dab509f5cc92e51
sqVish99 <vmharish@asu.edu>
2024-08-17 02:04:59 +0530

    adding blog link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import './Footer.css';
-import email from '../../assets/images/email.svg';
-import linkedin from '../../assets/images/linkedin.svg';
-import github from '../../assets/images/github.svg';
+import React from "react";
+import "./Footer.css";
+import email from "../../assets/images/email.svg";
+import linkedin from "../../assets/images/linkedin.svg";
+import github from "../../assets/images/github.svg";
+import blog from "../../assets/images/blog.png";
 
 const Footer = () => {
-    return (
-        <div className="portfolio-footer">
-            <div className="footer-content">
-                <a href="mailto:vmharish@asu.edu"><img className="footer-logo" src={email} alt='email' /></a>
-                <a href="https://www.linkedin.com/in/vishwas-m-h/" target="_blank" rel="noopener noreferrer"><img src={linkedin} className="footer-logo" alt='linkedin' /></a>
-                <a className="dark-icon" href="https://github.com/sqVish99" target="_blank" rel="noopener noreferrer"><img src={github} className="footer-logo" alt='github' /></a>
-                
-            </div>
-        </div>
-    );
+  return (
+    <div className="portfolio-footer">
+      <div className="footer-content">
+        <a href="mailto:vmharish@asu.edu">
+          <img className="footer-logo" src={email} alt="email" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/vishwas-m-h/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedin} className="footer-logo" alt="linkedin" />
+        </a>
+        <a
+          className="dark-icon"
+          href="https://github.com/sqVish99"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={github} className="footer-logo" alt="github" />
+        </a>
+        <a
+          className="dark-icon"
+          href="https://sqvish99.github.io/thoughtfulreveries/#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={blog} className="footer-logo" alt="Blog" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
